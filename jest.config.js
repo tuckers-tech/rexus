24c830a67c6a31a00abe560c4763a80da421f647
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-06-12 11:33:40 -0400

    updated tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+if (process.argv.includes("frontend")) {
+  module.exports = {
+    preset: "@vue/cli-plugin-unit-jest",
+    testMatch: [
+      "**/tests/frontend/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[jt]s?(x)",
+    ],
+  };
+} else {
+  module.exports = {
+    testMatch: [
+      "**/tests/backend/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[jt]s?(x)",
+    ],
+  };
+}
---
+++ tests/backend/unit/example.spec.js
@@ -0,0 +1,5 @@
+describe("Testing", () => {
+  test("2 = 2", () => {
+    expect(2).toBe(2);
+  });
+});
