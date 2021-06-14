28e7428199372692f9785b28f54e0925a007612f
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-06-14 19:40:24 -0400

    refactored FE testing

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,4 @@
-if (process.argv.includes("frontend")) {
-  module.exports = {
-    preset: "@vue/cli-plugin-unit-jest",
-    testMatch: [
-      "**/tests/frontend/**/*.[jt]s?(x)",
-      "**/?(*.)+(spec|test).[jt]s?(x)",
-    ],
-  };
-} else {
-  module.exports = {
-    testMatch: [
-      "**/tests/backend/**/*.[jt]s?(x)",
-      "**/?(*.)+(spec|test).[jt]s?(x)",
-    ],
-  };
-}
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+};
