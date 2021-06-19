38770c8a1b625eed08d5a0ff095d12190e276f72
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-06-19 10:35:56 -0400

    set up basic file server and api endpoint

--- server.go
+++ server.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-func main() {
-    
-    // rdb := redis.NewClient(&redis.Options{
-    //     Addr:     "localhost:6379",
-    //     Password: "", // no password set
-    //     DB:       0,  // use default DB
-    // })
+type Article struct {
+    Title string `json:"Title"`
+    Desc string `json:"desc"`
+    Content string `json:"content"`
+}
 
-    http.Handle("/", http.FileServer(http.Dir("./static")))
+func getServerStatus(w http.ResponseWriter, r *http.Request) {
+    Articles := []Article{
+        {Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        {Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
+}
+
+
+func watchRoutes() {
+    
+    fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fs)
+    http.HandleFunc("/status", getServerStatus)
+    fmt.Println("Handling Requests on http://localhost:4375")
     http.ListenAndServe(":4375", nil)
+    // log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+    watchRoutes()
 }
