380d30529c8e0b299de38cf636d09cbb090df2ef
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2021-06-22 21:28:10 -0400

    removed extra data

--- src/components/Modals/AddConnectionModal.vue
+++ src/components/Modals/AddConnectionModal.vue
@@ -154,19 +154,12 @@ export default {
           `${window.API_LOCATION}/api/v1/connection/test`,
           {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-``cors, *cors, same-origin
-            // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            // credentials: 'same-origin', // include, *same-origin, omit
             headers: {
               'Content-Type': 'application/json',
-              // 'Content-Type': 'application/x-www-form-urlencoded',
             },
-            // redirect: 'follow', // manual, *follow, error
-            // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             body: JSON.stringify(this.connection), // body data type must match "Content-Type" header
           },
         ).then((response) => response.json());
-        console.log(results);
         if (results.connectionValid) {
           this.connectionTestStatus = 'success';
         } else {
